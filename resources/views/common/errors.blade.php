7ecd2dbc88808a9c9659284443cfffb47845daa6
NguyenVinhHien <vinhhient2@gmail.com>
2018-10-12 13:48:27 +0700

    init project

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,16 @@
+@include('common.errors')
+
+@if (count($errors) > 0)
+    <!-- Form Error List -->
+    <div class="alert alert-danger">
+        <strong>Whoops! Something went wrong!</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
